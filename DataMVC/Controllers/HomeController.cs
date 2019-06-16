e1e9f487e0ad3ed4bc4b98b32aa472a387ce9cd9
Gabriel Bury <gabrielbury@hotmail.com>
2019-06-16 12:28:53 -0300

    first commit

---
+++ DataMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection dados)
+        {
+            var cultureInfo = new System.Globalization.CultureInfo("pt-Br");
+            DateTime data = DateTime.Parse(dados["txtData"], cultureInfo);
+
+            bool feriado = dados.AllKeys.Any(k => k == "chkFeriado");
+
+            using (var ctx = new Models.DataMVCEntities())
+            {
+                ctx.Data.Add(new Models.Datum
+                {
+                    Data = data,
+                    Feriado = feriado
+                });
+                ctx.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+}
